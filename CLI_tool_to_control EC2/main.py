861fedb7210752df2ebf5a40b24d295732afa3a4
ManuL <Velozu97aproved@gmail.com>
2023-03-04 12:22:31 +0200

    create first steps to product

---
+++ CLI_tool_to_control EC2/main.py
@@ -0,0 +1,20 @@
+
+class Service:
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def list(self):
+        pass
+
+
+class DataInstance:
+    def main(self):
+        pass
+
+
+class WorkWithBoto3:
+    def main(self):
+        pass
