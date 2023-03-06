aa2ac124947fa915ca4541c2a06d9ae3d8b0ca7a
ManuL <Velozu97aproved@gmail.com>
2023-03-06 21:33:38 +0200

    improve main.py

--- cli_tool_to_control_ec2/main.py
+++ cli_tool_to_control_ec2/main.py
@@ -3,8 +3,8 @@ import click
 
 
 class EC2Service:
-    def __init__(self):
-        self.ec2 = boto3.client('ec2')
+    def __init__(self, ec2_client):
+        self.ec2 = ec2_client
 
     def start_instances(self, image_id, instance_type, key_name, security_group, subnet_id, count=1):
         """Start EC2 instances"""
@@ -20,6 +20,14 @@ class EC2Service:
 
         for instance in instances['Instances']:
             print(f"Instance {instance['InstanceId']} started.")
+
+    def start_existing_instance(self, instance_id):
+        """Start an existing EC2 instance"""
+        response = self.ec2.describe_instances(
+            InstanceIds=[instance_id],
+            DryRun=False
+        )
+        print(f"Instance {instance_id} started with response {response['ResponseMetadata']['HTTPStatusCode']}.")
 
     def stop_instances(self, instance_id):
         """Stop EC2 instances"""
@@ -40,57 +48,59 @@ class EC2Service:
                 print("-------------------------------")
 
 
-class EC2Manager:
-    def __init__(self, ec2_service):
-        self.ec2_service = ec2_service
+class EC2Client:
+    def __init__(self):
+        self.ec2_manager = EC2Service(boto3.client('ec2'))
+        self.count = None
+        self.image_id = None
+        self.instance_type = None
+        self.instance_id = None
+        self.key_name = None
+        self.security_group = None
+        self.subnet_id = None
 
-    def start_existing_instance(self, instance_id):
+    @click.group()
+    @click.pass_context
+    def cli(ctx):
+        """Command line interface for EC2 management"""
+        ctx.obj = EC2Client()
+
+    @cli.command()
+    @click.option('--count', default=1, help='Number of instances to start')
+    @click.option('--image-id', required=True, help='AMI image ID')
+    @click.option('--instance-type', default='t2.micro', help='Instance type')
+    @click.option('--key-name', required=True, help='Key pair name')
+    @click.option('--security-group', required=True, help='Security group ID')
+    @click.option('--subnet-id', required=True, help='Subnet ID')
+    @click.pass_obj
+    def start_instances(self, obj, count, image_id, instance_type, key_name, security_group, subnet_id):
+        obj.count = count
+        obj.image_id = image_id
+        obj.instance_type = instance_type
+        obj.security_group = security_group
+        obj.subnet_id = subnet_id
+        """Start EC2 instances"""
+        obj.ec2_manager.start_instances(image_id, instance_type, key_name, security_group, subnet_id, count)
+
+    @cli.command()
+    @click.option('--instance-id', required=True, help='EC2 instance ID')
+    @click.pass_obj
+    def start_existing_instance(self, obj, ec2_manager, instance_id):
+        obj.ec2_manager = ec2_manager
+        obj.instance_id = instance_id
         """Start an existing EC2 instance"""
-        response = self.ec2_service.ec2.start_instances(
-            InstanceIds=[instance_id],
-            DryRun=False
-        )
-        print(f"Instance {instance_id} started with response {response['ResponseMetadata']['HTTPStatusCode']}.")
+        obj.ec2_manager.start_existing_instance(instance_id)
 
+    @cli.command()
+    @click.option('--instance-id', required=True, help='EC2 instance ID')
+    @click.pass_obj
+    def stop_instance(self, obj, instance_id):
+        """Stop an EC2 instance"""
+        obj.ec2_manager.stop_instances(instance_id)
 
-@click.group()
-@click.pass_context
-def cli(ctx):
-    """Command line interface for EC2 management"""
-    ctx.obj = EC2Manager(EC2Service())
+    @cli.command()
+    @click.pass_obj
+    def list_instances(self, obj):
+        """List all EC2 instances"""
+        obj.ec2_manager.list_instances()
 
-
-@cli.command()
-@click.option('--count', default=1, help='Number of instances to start')
-@click.option('--image-id', required=True, help='AMI image ID')
-@click.option('--instance-type', default='t2.micro', help='Instance type')
-@click.option('--key-name', required=True, help='Key pair name')
-@click.option('--security-group', required=True, help='Security group ID')
-@click.option('--subnet-id', required=True, help='Subnet ID')
-@click.pass_obj
-def start_instances(ec2_manager, count, image_id, instance_type, key_name, security_group, subnet_id):
-    """Start EC2 instances"""
-    ec2_manager.ec2_service.start_instances(image_id, instance_type, key_name, security_group, subnet_id, count)
-
-
-@cli.command()
-@click.option('--instance-id', required=True, help='EC2 instance ID')
-@click.pass_obj
-def start_existing_instance(ec2_manager, instance_id):
-    """Start an existing EC2 instance"""
-    ec2_manager.start_existing_instance(instance_id)
-
-
-@cli.command()
-@click.option('--instance-id', required=True, help='EC2 instance ID')
-@click.pass_obj
-def stop_instances(ec2_manager, instance_id):
-    """Stop EC2 instances"""
-    ec2_manager.ec2_service.stop_instances(instance_id)
-
-
-@cli.command()
-@click.pass_obj
-def list_instances(ec2_manager):
-    """List all EC2 instances"""
-    ec2_manager.ec2_service.list_instances()
