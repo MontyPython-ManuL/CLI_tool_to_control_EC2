f056ef6b50795129f36ab2d35bc69a47606c0549
ManuL <Velozu97aproved@gmail.com>
2023-03-17 11:08:26 +0200

    try up coverage

--- tests/test_ec2_service.py
+++ tests/test_ec2_service.py
@@ -1,6 +1,7 @@
 import boto3
 from unittest.mock import patch
 import unittest
+from moto import mock_ec2
 from cli_tool_to_control_ec2.main import EC2Service, EC2Management
 
 session = boto3.Session()
@@ -32,7 +33,6 @@ class TestEC2Service(unittest.TestCase):
 class TestEC2Management(unittest.TestCase):
     @patch.object(EC2Service, 'start_existing_instance')
     def test_start_existing_instance(self, mock_start):
-        instance_id = 'i-0f587a48713a80472'
         management = EC2Management(ec2, instance_id)
         management.start_existing_instance()
         mock_start.assert_called_once()
@@ -48,6 +48,29 @@ class TestEC2Management(unittest.TestCase):
         management = EC2Management(ec2, instance_id)
         management.list_instances()
         mock_list.assert_called_once()
+
+    def test_start_existing_instance_with_invalid_state(self):
+        instance_id = 'i-0123456789abcdef0'
+        response = EC2Service(ec2, instance_id).start_existing_instance()
+        assert response == f"An error occurred while starting the instance. Error: The instance '{instance_id}' is in a state that is not valid for this operation."
+        ec2.close()
+
+    def test_stop_instance_with_invalid_id(self):
+        invalid_instance_id = 'invalid_id'
+        response = EC2Service(ec2, invalid_instance_id).stop_instance()
+        assert response == f"An error occurred while stopping the instance. Error: The instance ID '{invalid_instance_id}' does not exist"
+        ec2.close()
+
+    def test_ec2management_with_invalid_id(self):
+        invalid_instance_id = 'invalid_id'
+        management = EC2Management(ec2, invalid_instance_id)
+        response = management.start_existing_instance()
+        assert response == f"An error occurred while starting the instance. Error: The instance ID '{invalid_instance_id}' does not exist"
+        response = management.stop_instance()
+        assert response == f"An error occurred while stopping the instance. Error: The instance ID '{invalid_instance_id}' does not exist"
+        response = management.list_instances()
+        assert response == f"An error occurred while listing instances. Error: The instance ID '{invalid_instance_id}' does not exist"
+        ec2.close()
 
 
 if __name__ == '__main__':
